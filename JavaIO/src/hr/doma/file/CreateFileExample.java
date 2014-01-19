4addca1d932e1f1f1141c12f38564f13a56cc494
Rin0 <rinokovacevic@gmail.com>
2014-01-19 13:03:35 +0100

    1. comit!

---
+++ JavaIO/src/hr/doma/file/CreateFileExample.java
@@ -0,0 +1,24 @@
+package hr.doma.file;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CreateFileExample {
+
+	public static void main(String[] args) {
+
+		try {
+			File file = new File("c:\\fer\\newjavafile.txt");
+
+			if (file.createNewFile()) {
+				System.out.println("File is created");
+			} else {
+				System.out.println("File already exists");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
